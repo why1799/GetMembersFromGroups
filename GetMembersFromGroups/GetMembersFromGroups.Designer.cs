249573a73fb40d0bb3d7b92eefaa2160def69fa9
why1799 <33399450+why1799@users.noreply.github.com>
2019-02-15 22:09:24 +0300

    Add files via upload

---
+++ GetMembersFromGroups/GetMembersFromGroups.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace GetMembersFromGroups
+{
+    partial class GetMembersFromGroups
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.open = new System.Windows.Forms.Button();
+            this.pathbox = new System.Windows.Forms.TextBox();
+            this.intersectionlabel = new System.Windows.Forms.Label();
+            this.intersectionbox = new System.Windows.Forms.TextBox();
+            this.statuslabel1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.start = new System.Windows.Forms.Button();
+            this.logout = new System.Windows.Forms.Button();
+            this.statuslabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(244, 10);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 10;
+            this.open.Text = "Обзор";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // pathbox
+            // 
+            this.pathbox.Location = new System.Drawing.Point(12, 13);
+            this.pathbox.Name = "pathbox";
+            this.pathbox.ReadOnly = true;
+            this.pathbox.Size = new System.Drawing.Size(226, 20);
+            this.pathbox.TabIndex = 9;
+            this.pathbox.TabStop = false;
+            // 
+            // intersectionlabel
+            // 
+            this.intersectionlabel.AutoSize = true;
+            this.intersectionlabel.Location = new System.Drawing.Point(9, 49);
+            this.intersectionlabel.Name = "intersectionlabel";
+            this.intersectionlabel.Size = new System.Drawing.Size(128, 13);
+            this.intersectionlabel.TabIndex = 11;
+            this.intersectionlabel.Text = "Пересечение в группах:";
+            // 
+            // intersectionbox
+            // 
+            this.intersectionbox.Location = new System.Drawing.Point(244, 46);
+            this.intersectionbox.Name = "intersectionbox";
+            this.intersectionbox.Size = new System.Drawing.Size(75, 20);
+            this.intersectionbox.TabIndex = 12;
+            this.intersectionbox.Text = "2";
+            // 
+            // statuslabel1
+            // 
+            this.statuslabel1.AutoSize = true;
+            this.statuslabel1.Location = new System.Drawing.Point(96, 124);
+            this.statuslabel1.Name = "statuslabel1";
+            this.statuslabel1.Size = new System.Drawing.Size(0, 13);
+            this.statuslabel1.TabIndex = 13;
+            this.statuslabel1.TextChanged += new System.EventHandler(this.statuslabel1_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(14, 169);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(305, 23);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(244, 72);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 15;
+            this.start.Text = "Начать";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // logout
+            // 
+            this.logout.Location = new System.Drawing.Point(227, 243);
+            this.logout.Name = "logout";
+            this.logout.Size = new System.Drawing.Size(92, 23);
+            this.logout.TabIndex = 16;
+            this.logout.Text = "Разлогиниться";
+            this.logout.UseVisualStyleBackColor = true;
+            this.logout.Click += new System.EventHandler(this.logout_Click);
+            // 
+            // statuslabel2
+            // 
+            this.statuslabel2.AutoSize = true;
+            this.statuslabel2.Location = new System.Drawing.Point(139, 143);
+            this.statuslabel2.Name = "statuslabel2";
+            this.statuslabel2.Size = new System.Drawing.Size(0, 13);
+            this.statuslabel2.TabIndex = 17;
+            this.statuslabel2.TextChanged += new System.EventHandler(this.statuslabel2_TextChanged);
+            // 
+            // GetMembersFromGroups
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 282);
+            this.Controls.Add(this.statuslabel2);
+            this.Controls.Add(this.logout);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statuslabel1);
+            this.Controls.Add(this.intersectionbox);
+            this.Controls.Add(this.intersectionlabel);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.pathbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "GetMembersFromGroups";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GetMembersFromGroups";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.TextBox pathbox;
+        private System.Windows.Forms.Label intersectionlabel;
+        private System.Windows.Forms.TextBox intersectionbox;
+        private System.Windows.Forms.Label statuslabel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button logout;
+        private System.Windows.Forms.Label statuslabel2;
+    }
+}
+
---
+++ GetMembersFromGroups/GetMembersFromGroups.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading;
+
+namespace GetMembersFromGroups
+{
+    public partial class GetMembersFromGroups : Form
+    {
+        public GetMembersFromGroups()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "Выберите файл";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathbox.Text = openFileDialog1.FileName.ToString();
+            }
+        }
+
+        private void logout_Click(object sender, EventArgs e)
+        {
+            Auth.LogOut();
+            Close();
+        }
+
+        private void DisEnb(bool action)
+        {
+            open.Enabled = action;
+            start.Enabled = action;
+            logout.Enabled = action;
+            pathbox.Enabled = action;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int.Parse(intersectionbox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Значение должно быть целым числом!");
+                return;
+            }
+
+            string[] readText;
+            List<string> ids = new List<string>();
+            try
+            {
+                readText = File.ReadAllLines(pathbox.Text);
+                foreach (string a in readText)
+                {
+                    int i = a.IndexOf("club");
+                    if (i == -1)
+                    {
+                        i = a.IndexOf("public");
+                        if (i == -1)
+                        {
+                            MessageBox.Show("Неправильный формат групп");
+                            return;
+                        }
+                        else
+                            i += 6;
+                    }
+                    else
+                        i += 4;
+
+                    ids.Add(a.Substring(i));
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Считать файл не удалось!");
+                return;
+            }
+
+            DisEnb(false);
+
+            StreamWriter writer = new StreamWriter("out.txt");
+
+            foreach(var x in GetMembers(ids))
+            {
+                if(x.Value == int.Parse(intersectionbox.Text))
+                {
+                    writer.WriteLine(x.Key);
+                }
+            }
+            writer.Close();
+
+            DisEnb(true);
+        }
+
+        private SortedDictionary<string, int> GetMembers(List<string> getfromgroups)
+        {
+            SortedDictionary<string, int> members = new SortedDictionary<string, int>();
+
+            statuslabel1.Text = "Выгрузка участников из групп";
+
+            int[] offset = new int[25];
+            bool[] use = new bool[25];
+            for (int i = 0; i < 25; i++)
+            {
+                offset[i] = 0;
+                use[i] = false;
+            }
+
+            string[] groupids = new string[25];
+
+            int nowmember = 0;
+            for (; nowmember < 25 && nowmember < getfromgroups.Count(); nowmember++)
+            {
+                groupids[nowmember] = getfromgroups[nowmember];
+                use[nowmember] = true;
+            }
+
+
+
+            string code = "";
+            bool again = false;
+            int finished = 0;
+
+            while (finished < getfromgroups.Count())
+            {
+                statuslabel2.Text = finished + " из " + getfromgroups.Count() + " групп";
+                progressBar1.Value = finished * 100 / getfromgroups.Count();
+                Application.DoEvents();
+
+                if (!again)
+                {
+                    code = "";
+                    for (int i = 0; i < 25; i++)
+                    {
+                        if (use[i])
+                            code += String.Format($"var {(char)(i + 'a')}=API.groups.getMembers(") + "{" + String.Format($"\"group_id\":\"{groupids[i]}\",\"offset\":\"{offset[i] * 1000}\",\"count\":\"1000\"") + "});";
+                        offset[i]++;
+                    }
+
+                    code += "return [";
+
+                    for (int i = 0; i < 25; i++)
+                    {
+                        if (use[i])
+                            code += (char)(i + 'a') + ",";
+                    }
+
+                    code += "];";
+                }
+
+                again = false;
+                string got = get("https://api.vk.com/method/execute?code=" + code + "&access_token=" + Prog.Token + "&v=" + Prog.Version).ToString();
+
+
+                string save = got;
+                got = got.Substring(12);
+                try
+                {
+                    got = got.Remove(got.IndexOf(",\"execute_errors\""));
+                }
+                catch { }
+
+                while (got.IndexOf("false") != -1)
+                {
+                    got = got.Replace("false", "{\"count\":0,\"items\":[]}");
+                }
+
+
+                MembersResponse[] membersResponses;
+                try
+                {
+                    using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(got)))
+                    {
+                        DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(MembersResponse[]));
+                        membersResponses = (MembersResponse[])jsonFormatter.ReadObject(ms);
+                    }
+                }
+                catch
+                {
+                    /*ErrorResponse error;
+                    using (var ms = new MemoryStream(Encoding.Unicode.GetBytes("[" + save + "]")))
+                    {
+                        DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(ErrorResponse));
+                        error = (ErrorResponse)jsonFormatter.ReadObject(ms);
+                    }*/
+
+                    int s = save.IndexOf("{\"error\":{\"error_code\":6,\"error_msg");
+                    if (s == 0)
+                    {
+                        Thread.Sleep(500);
+                    }
+
+                    again = true;
+                    continue;
+                }
+
+                for (int i = 0, j = 0; i < 25; i++, j++)
+                {
+                    for (; j < 25 && !use[j]; j++) ;
+
+                    if (j == 25)
+                        break;
+
+                    foreach (int ids in membersResponses[i].items)
+                    {
+                        if (members.ContainsKey(ids.ToString()))
+                        {
+                            members[ids.ToString()]++;
+                        }
+                        else
+                        {
+                            members.Add(ids.ToString(), 1);
+                        }
+                    }
+
+                    if (membersResponses[i].items.Count() < 1000)
+                    {
+                        finished++;
+                        offset[j] = 0;
+                        if (nowmember == getfromgroups.Count())
+                        {
+                            use[j] = false;
+                        }
+                        else
+                        {
+                            groupids[j] = getfromgroups[nowmember];
+                            nowmember++;
+                        }
+                    }
+                }
+            }
+
+            statuslabel1.Text = "";
+            statuslabel2.Text = "Участники были выгружены";
+            progressBar1.Value = 100;
+
+            return members;
+        }
+
+        private static string get(string Url)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(Url);
+            System.Net.WebResponse resp = req.GetResponse();
+            System.IO.Stream stream = resp.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(stream);
+            string Out = sr.ReadToEnd();
+            sr.Close();
+            return Out;
+        }
+
+        private void statuslabel1_TextChanged(object sender, EventArgs e)
+        {
+            //statuslabel1.Left = this.Width / 2 - statuslabel1.Width / 2;
+            Application.DoEvents();
+        }
+
+        private void statuslabel2_TextChanged(object sender, EventArgs e)
+        {
+            statuslabel2.Left = this.Width / 2 - statuslabel2.Width / 2;
+            Application.DoEvents();
+        }
+    }
+}
